86989eb1a81beb68f4703704f4ada7f5622f6ca9
Vytenis Bivainis <monrealis@gmail.com>
2015-04-23 23:25:54 +0300

    Create first class.

--- examples/src/hello.groovy
+++ examples/src/hello.groovy
@@ -1,8 +1,4 @@
-strings();
-
-def hello() {
-    printf "Hello World!\n"
-}
+new HelloWorld().sayHello()
 
 def loops() {
     (1..10).each { i ->
@@ -15,4 +11,10 @@ def strings() {
     def a = 10
     println '$a'
     println "$a"
+}
+
+class HelloWorld {
+    def sayHello() {
+        printf "Hello World!\n"
+    }
 }
