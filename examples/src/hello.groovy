0eab092e0b6e5eaad18856e08b6e8d26060999ba
Vytenis Bivainis <monrealis@gmail.com>
2015-04-30 21:34:28 +0300

    Add tests of collections.

--- examples/src/hello.groovy
+++ examples/src/hello.groovy
@@ -1,9 +1,8 @@
 #!/usr/bin/env groovy
 
 new HelloWorld().setName("WORLD").sayHello()
-maps();
-strings();
-pass();
+maps()
+collections()
 
 def loops() {
     (1..10).each { i ->
@@ -30,6 +29,17 @@ def pass() {
 
 def fail() {
     assert false
+}
+
+void collections() {
+    def l = [1, 2, 3]
+    int[] a = [4, 5, 6]
+    Integer[] a2 = [4, 5, 6]
+    def m = [1: 2, 3: 4]
+    println '' + l + ' ' + l.getClass()
+    println '' + a + ' ' + a.getClass()
+    println '' + m + ' ' + m.getClass()
+    println "$a2 ${a2.getClass()}"
 }
 
 class HelloWorld {
